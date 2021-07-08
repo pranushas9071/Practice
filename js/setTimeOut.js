570283196b73088a80665ff3002b6ded5b225d57
pranushas9071 <pranusha437@gmail.com>
2021-07-08 13:06:45 +0530

    Reformatted the files

--- js/setTimeOut.js
+++ js/setTimeOut.js
@@ -1,5 +1,24 @@
 console.log("First part..");
-setTimeout(function () {
-  console.log("Second part..");
-}, 500);
+function run() {
+  setTimeout(function () {
+    console.log("Second part..");
+  }, 500);
+}
+run();
 console.log("Third part..");
+
+let a=[1,2,3,4,5];
+for (var i = 0; i < a.length; i++) {
+  setTimeout(() => {
+  console.log(i);
+  console.log(a[i]);
+  }, 2000);
+}
+
+let i=1;
+setInterval(() => {
+  if(i<6){
+    console.log(i);
+    i++;
+  }
+}, 2000);
